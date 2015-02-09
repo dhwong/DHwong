4ff54658b1fbefd3681cb2184ddd348915171e59
dhwong <dhwong@smu.edu>
2015-02-09 03:01:56 -0600

    A4

--- site/js/A4.js
+++ site/js/A4.js
@@ -1,24 +1,22 @@
 function setBG() {
-    
-    switch(Math.floor(Math.random() * 6) + 1) { //generate a number between 0&9
-        case 1:
-            document.body.style.background = "url('../bg/BG1.png')";
-            break;
-        case 2:
-            document.body.style.background = "url('../bg/BG2.png')";
-            break;
-        case 3:
-            document.body.style.background = "url('../bg/BG3.png')";
-            break;
-        case 4:
-            document.body.style.background = "url('../bg/BG4.png')";
-            break;
-        case 5:
-            document.body.style.background = "url('../bg/BG4.png')";
-            break;
-        default:
-            document.body.style.background = "url('../bg/BG0.jpg')";
-            break;
+    var rand = Math.ceil(Math.random() * 6); //generate a number between 1&6
+    if (rand === 1) {
+        document.body.style.background = "black url('bg/BG1.png')";
+    }
+    else if (rand === 2) {        
+        document.body.style.background = "black url('bg/BG2.png')";
+    }
+    else if (rand === 3) {
+        document.body.style.background = "black url('bg/BG3.png')";
+    }
+    else if (rand === 4) {
+        document.body.style.background = "black url('bg/BG4.png')";
+    }
+    else if (rand === 5) {
+        document.body.style.background = "black url('bg/BG4.png')";
+    }
+    else {
+        document.body.style.background = "black url('bg/BG0.jpg')";
     }
 }
 
